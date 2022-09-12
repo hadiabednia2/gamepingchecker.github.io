9cfde655775f59e5da5a20ccde097373e801e713
jenyorsta <jenyorsata@gmail.com>
2022-09-13 02:07:18 +0430

    updated

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
-const pingBtn = document.querySelector('button')
-  const showPing = document.querySelector('p')
-    setInterval(function(){
-      let xhttp = new XMLHttpRequest()
-      xhttp.open('GET','https://dynamodb.me-south-1.amazonaws.com')
-      let clickDate = new Date().getTime()
-      xhttp.onload = function(){
-          let responeTime = new Date().getTime()
-          let pingRes = (responeTime - clickDate) - 20
-          showPing.innerHTML = pingRes
-          console.log(pingRes);
-      }
-      xhttp.send()
-          
-      },1000)
+const bahrainPingBtn = document.getElementById('bahrain')
+const germanPingBth = document.getElementById('euCenteral')
+const euWestpingBtn = document.getElementById('euWest')
+const euSouthpingBtn = document.getElementById('euSouth')
+const londonpingBtn = document.getElementById('london')
+const showPing = document.querySelector('p')
+
+function bahrainPingTest(){
+  setInterval(function(){
+    pingTest('GET','https://dynamodb.me-south-1.amazonaws.com')
+        
+    },1000)
+}
+function germanPingTets(){
+  setInterval(function(){
+    pingTest('GET','https://dynamodb.eu-central-1.amazonaws.com')
+        
+    },1000)
+}
+function euWestPingTets(){
+  setInterval(function(){
+    pingTest('GET','https://dynamodb.eu-west-1.amazonaws.com')
+        
+    },1000)
+}
+function euSouthPingTets(){
+  setInterval(function(){
+    pingTest('GET','https://dynamodb.eu-south-1.amazonaws.com')
+        
+    },1000)
+}
+function londonPingTets(){
+  setInterval(function(){
+    pingTest('GET','https://dynamodb.eu-west-2.amazonaws.com')
+        
+    },1000)
+}
+
+function pingTest(method,url){
+  let xhttp = new XMLHttpRequest()
+  xhttp.open(method,url)
+  let clickDate = new Date().getTime()
+  xhttp.onload = function(){
+      let responeTime = new Date().getTime()
+      let pingRes = (responeTime - clickDate) - 20
+      showPing.innerHTML = pingRes
+      console.log(pingRes);
+  }
+  xhttp.send()
+}
+bahrainPingBtn.addEventListener('click',bahrainPingTest)
+germanPingBth.addEventListener('click',germanPingTets)    
+germanPingBth.addEventListener('click',euWestPingTets)    
+germanPingBth.addEventListener('click',euSouthPingTets)    
+germanPingBth.addEventListener('click',londonPingTets)   
+ 
