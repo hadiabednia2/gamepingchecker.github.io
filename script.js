d77e90795b7af1cfaedd25b3c1e53b19918a2cd6
hadi abednia <hadiabednia@gmail.com>
2022-09-14 17:50:48 +0430

    new responsive updated

--- script.js
+++ script.js
@@ -9,35 +9,35 @@
 //   clearInterval(intervalID)
 //   intervalID = setInterval(function(){
 //     pingTest('GET','https://dynamodb.me-south-1.amazonaws.com','bahrain Ping: ')
-        
+
 //     },1000)
 // }
 // function germanPingTets(){
 //   clearInterval(intervalID)
 //   intervalID = setInterval(function(){
 //     pingTest('GET','https://dynamodb.eu-central-1.amazonaws.com','euCentral Ping:')
-    
+
 //   },1000)
 // }
 // function euWestPingTets(){
 //   clearInterval(intervalID)
 //   intervalID = setInterval(function(){
 //     pingTest('GET','https://dynamodb.eu-west-1.amazonaws.com','euWest Ping: ')
-    
+
 //   },1000)
 // }
 // function euSouthPingTets(){
 //   clearInterval(intervalID)
 //   intervalID = setInterval(function(){
 //     pingTest('GET','https://dynamodb.eu-south-1.amazonaws.com','euSouth Ping: ')
-    
+
 //   },1000)
 // }
 // function londonPingTets(){
 //   clearInterval(intervalID)
 //   intervalID = setInterval(function(){
 //     pingTest('GET','https://dynamodb.eu-west-2.amazonaws.com','euWest2 Ping: ')
-    
+
 //   },1000)
 // }
 
@@ -53,10 +53,27 @@
 //   xhttp.send()
 // }
 // bahrainPingBtn.addEventListener('click',bahrainPingTest)
-// germanPingBth.addEventListener('click',germanPingTets)    
-// euWestpingBtn.addEventListener('click',euWestPingTets)    
-// euSouthpingBtn.addEventListener('click',euSouthPingTets)    
-// londonpingBtn.addEventListener('click',londonPingTets)   
+// germanPingBth.addEventListener('click',germanPingTets)
+// euWestpingBtn.addEventListener('click',euWestPingTets)
+// euSouthpingBtn.addEventListener('click',euSouthPingTets)
+// londonpingBtn.addEventListener('click',londonPingTets)
+
+const navbarBox = document.querySelector('.items')
+const opnMenuBtn = document.querySelector('.fa-bars')
+const closeMenuBtn = document.querySelector('.fa-xmark')
+console.log(opnMenuBtn);
+function openMenu(){
+  console.log('hello');
+  navbarBox.style.display = 'block'
+}
+function closeMenu(){
+
+  navbarBox.style.display = 'none'
+}
+opnMenuBtn.addEventListener('click',openMenu)
+closeMenuBtn.addEventListener('click',closeMenu)
 window.addEventListener('resize',function(){
-  console.log('resize');
+  if(window.innerWidth > 408){
+    navbarBox.style.display = 'none'
+  }
 })
