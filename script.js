c8c585d22143a7ccac967aa19b3b1659fee698d7
hadi abednia <hadiabednia@gmail.com>
2022-09-15 22:55:28 +0430

    website updated

--- script.js
+++ script.js
@@ -61,6 +61,24 @@
 const navbarBox = document.querySelector('.items')
 const opnMenuBtn = document.querySelector('.fa-bars')
 const closeMenuBtn = document.querySelector('.fa-xmark')
+const gameListContainer = document.querySelector('.gameLists')
+let gameListArray = [{
+  id:1,name:'league of legends',img:'./gameimg/leagueOfLegends.jpg',alt:'leagueOfLenegds'},
+  {id:2,name:'valorant',img:'./gameimg/valorant.jpg',alt:'valorant'},
+  {id:3,name:'dota2',img:'./gameimg/dota2.jpg',alt:'dota2'},
+  {id:4,name:'csgo',img:'./gameimg/csgo.jpg',alt:'csgo'},
+  {id:5,name:'pubg mobile',img:'./gameimg/pubgmobile.jpg',alt:'pubgmobile'},
+  {id:6,name:'warzone',img:'./gameimg/warzone.jpg',alt:'warzone'},
+  {id:7,name:'apex legends',img:'./gameimg/apexlegends.jpg',alt:'apexlegends'},
+  {id:8,name:'fortnite',img:'./gameimg/fortnite.jpg',alt:'fortnite'},
+  {id:9,name:'world of warcraft',img:'./gameimg/wow.jpg',alt:'world of craft'},
+  {id:10,name:'pubg',img:'./gameimg/pubg.jpg',alt:'pubg'},
+  {id:11,name:'apex Legends Mobile',img:'./gameimg/apexMobile.jpg',alt:'apex Legends Mobile'},
+  {id:12,name:'overwatch',img:'./gameimg/overwatch.jpg',alt:'overwatch'},
+  {id:13,name:'rainbow six',img:'./gameimg/rainbow.jpg',alt:'rainbow'},
+  {id:14,name:'clash Royal',img:'./gameimg/clashRoyal.jpeg',alt:'clashRoyal'},
+  {id:15,name:'call of mobile',img:'./gameimg/callofmobile.jpg',alt:'call of mobile'},
+  {id:16,name:'mobile legends',img:'./gameimg/mobilelegends.png',alt:'mobile legends'},]
 console.log(opnMenuBtn);
 function openMenu(){
   console.log('hello');
@@ -77,3 +95,7 @@ window.addEventListener('resize',function(){
     navbarBox.style.display = 'none'
   }
 })
+gameListArray.forEach(function(game){
+  gameListContainer.insertAdjacentHTML('beforeend','<div class="games"><div class="imgContainer"><img src="'+game.img+'" alt="'+game.alt+'"></div><div class="desc"><p>See '+game.name+' ping</p></div></div>')
+})
+
